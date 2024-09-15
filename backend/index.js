ad91b134930afc002989113f81ebd9d5fc035507
Maxim Dudai <maxim.dudai01@gmail.com>
2024-09-15 15:56:34 +0100

    server

--- backend/index.js
+++ backend/index.js
@@ -2,42 +2,27 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const socketHandler = require('./app/socket/socket');
-const cors = require('cors');
-
 require('dotenv').config();
-
-// Import your routes
-const uploadRoutes = require('./app/routes/routes');
-
 const app = express();
 const server = http.createServer(app);
+const uploadRoutes = require('./app/routes/routes');
+
+
 const io = new Server(server, {
   cors: {
     origin: '*',
     methods: ['GET', 'POST'],
   },
 });
-
-// app.use(cors({
-//   origin: `${process.env.SERVER_IP}:${process.env.SERVER_PORT}`,
-//   methods: ['GET', 'POST'],
-// }));
-
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST'],
-}));
-
-// Initialize socket handler
 socketHandler(io);
 
 const PORT = process.env.PORT || 5050;
-const HOST = '0.0.0.0'; // Listen on all network interfaces
 
 server.listen(PORT, () => {
-  // console.log(`Server is running on http://${HOST}:${PORT}`);
   console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 // Use your routes
 app.use('/upload', uploadRoutes);
+
+
