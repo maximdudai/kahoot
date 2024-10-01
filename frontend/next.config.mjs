45d0ddcd4f9d99b29640cb6a12fce9c3af662aaf
maxdev <maxim.dudai01@gmail.com>
2024-10-01 21:22:02 +0100

    https -> http

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    SOCKET_URL: "https://51.178.18.74", // Make sure it's HTTPS
+    SOCKET_URL: "http://51.178.18.74", // Make sure it's HTTPS
 
   },
 };
