d4b095cdd18cf5db86544df5942015e254de603d
Maxim Dudai <maxim.dudai01@gmail.com>
2025-02-11 20:30:35 +0000

    nextjs config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    SOCKET_URL: "https://kahoot.pro/api",  // Use domain, not raw IP
+    SOCKET_URL: "https://kahoot.pro/api",
   },
-  distDir: "build", // Optional, default is ".next"
   reactStrictMode: true,
   poweredByHeader: false,
 };
